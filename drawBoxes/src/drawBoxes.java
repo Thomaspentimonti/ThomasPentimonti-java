1a4ecdf4f88ffdab9dfa70b804ecb1d34f1e1318
Thomas Pentimonti <20PentimontiThomas@bprep.org>
2018-09-10 09:13:48 -0700

    drawed boxes

---
+++ drawBoxes/src/drawBoxes.java
@@ -0,0 +1,29 @@
+public class drawBoxes {
+    public static void main(String[] args) {
+
+        drawbox();
+        drawbox();
+        drawbox();
+        System.out.println("complete");
+    }
+
+    public static void drawbox() {
+        System.out.println();
+        drawTop();
+        drawMiddle();
+        drawTop();
+    }
+        public static void drawTop() {
+            System.out.println("+-------+");
+        }
+                public static void drawMiddle(){
+                System.out.println ("|       |");
+                System.out.println ("|       |");
+                System.out.println ("|       |");
+
+            }
+
+
+
+    }
+
