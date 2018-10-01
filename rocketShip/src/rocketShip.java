19731a49262c17e3f82c1bedace2c85908ac7bc2
Thomas Pentimonti <20PentimontiThomas@bprep.org>
2018-10-01 09:18:17 -0700

    rocket ship lab

---
+++ rocketShip/src/rocketShip.java
@@ -0,0 +1,26 @@
+/*
+ *****************************************************
+ * Thomas Pentimonti
+ * AP CS A
+ * Period 1
+ * October 1, 2018
+ * Project#2 Rocket Ship
+ *****************************************************
+ */
+
+public class rocketShip {
+
+    public static void main(String[] args) {
+        drawcone();
+    }
+
+    public static void drawcone() {
+
+            for (int j = 1; j <= 5; j++) {
+                System.out.println(" ");
+                for (int i = 1; i <= 1; i++) {
+                    System.out.print("/");
+            }
+        }
+    }
+}
