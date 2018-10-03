0e65e16d8f188393b566d3c0a6e34aef6493628a
Thomas Pentimonti <20PentimontiThomas@bprep.org>
2018-10-03 10:10:59 -0700

    rocket ship lab

--- rocketShip/src/rocketShip.java
+++ rocketShip/src/rocketShip.java
@@ -11,16 +11,38 @@
 public class rocketShip {
 
     public static void main(String[] args) {
-        drawcone();
+        drawconeFirst();
+
+        //drawconeSecond();
     }
 
-    public static void drawcone() {
+    public static void drawBoxOne() {
 
-            for (int j = 1; j <= 5; j++) {
-                System.out.println(" ");
-                for (int i = 1; i <= 1; i++) {
-                    System.out.print("/");
+        for (int i =1; i <= 3; i++) {
+            System.out.print("");
+            System.out.println(" ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print("/");
+                System.out.print("\\");
+
+                }
             }
+        }
+
+        public static void drawconeSecond(){
+        for (int i = 1; i<=6; i++){
+            System.out.println(" ");
+            for (int j = 1; j<=i; j++){
+                System.out.print("\\");
+            }
+        }
+        public static void drawconeFirst(){
+        for (int i = 1; i<=6; i-- ) {
+            System.out.print(" ");
+            for (int j = 1; j <= i; j++)
+                System.out.print("/");
+        }
         }
     }
 }
+
